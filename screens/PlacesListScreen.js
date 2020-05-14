ff48f57067c58548da748c320fe28b8e9fa9c7ce
dariocostanzo <darioc@xara.com>
2020-05-14 18:10:21 +0100

    Outputting a List of Places

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import { View, Text, StyleSheet, Platform } from 'react-native';
+import { View, Text, StyleSheet, Platform, FlatList } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector } from 'react-redux';
 
 import HeaderButton from '../components/HeaderButton';
+import PlaceItem from '../components/PlaceItem';
 
 const PlacesListScreen = props => {
+  const places = useSelector(state => state.places.places);
+  // I use state.place because I use "places" in App.js rootReducer
+  // that takes use to the slices managed with the places-reducer
+  // where we have another places key
   return (
-    <View>
-      <Text>PlacesListScreen</Text>
-    </View>
+    <FlatList
+      data={places}
+      keyExtractor={item => item.id}
+      renderItem={itemData => (
+        <PlaceItem
+          image={null}
+          title={itemData.item.title}
+          address={null}
+          onSelect={() => {
+            props.navigation.navigate('PlaceDetail', {
+              placeTitle: itemData.item.title,
+              placeId: itemData.item.id
+            });
+          }}
+        />
+      )}
+    />
   );
 };
 
-// Header title
 PlacesListScreen.navigationOptions = navData => {
   return {
     headerTitle: 'All Places',
-    headerRight: () => (
+    headerRight: (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
           title='Add Place'
