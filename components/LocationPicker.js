832821eee5c6ac65e467039b62245d2a6ad7d2d4
dariocostanzo <darioc@xara.com>
2020-05-16 17:59:33 +0100

    Showing a Map Preview of the Location

--- components/LocationPicker.js
+++ components/LocationPicker.js
@@ -12,6 +12,7 @@ import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 
 import Colors from '../constants/Colors';
+import MapPreview from './MapPreview';
 
 const LocationPicker = props => {
   const [isFetching, setIsFetching] = useState(false);
@@ -51,13 +52,13 @@ const LocationPicker = props => {
   };
   return (
     <View style={styles.locationPicker}>
-      <View style={styles.mapPreview}>
+      <MapPreview style={styles.mapPreview} location={pickedLocation}>
         {isFetching ? (
           <ActivityIndicator size='large' />
         ) : (
           <Text>No location chosen yet!</Text>
         )}
-      </View>
+      </MapPreview>
       <Button
         title='Get User Location'
         color={Colors.primary}
@@ -76,9 +77,7 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 150,
     borderColor: '#ccc',
-    borderWidth: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    borderWidth: 1
   }
 });
 
